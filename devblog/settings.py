0db390df2fb0cb9ea20a763bf4909c13a05feef8
abdurrahmanbinliakot <65095033+abdurrahmanbinliakot@users.noreply.github.com>
2020-09-06 04:32:06 +0600

    Update settings.py

--- devblog/settings.py
+++ devblog/settings.py
@@ -44,7 +44,11 @@ INSTALLED_APPS = [
 INSTALLED_APPS += ('django_summernote', )
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
+     # 'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+     # ...
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
